8a00dd2c49d4153c45979aa4b6ac5128f7c292d6
Josh Waldron <jwaldron@thejwal.com>
2015-02-02 23:16:03 -0600

    wasn't passing the css selector appropriately

--- src/addCheck.js
+++ src/addCheck.js
@@ -4,7 +4,7 @@ module.exports = {
         start(json["url"]);
         for(var i in json["transaction"]){
             var action = json["transaction"][i]["action"];
-            var selector = json["transaction"][i]["action"];
+            var selector = json["transaction"][i]["selector"];
             var text = json["transaction"][i]["text"];
             switch(action){
                 case "exist":
@@ -38,21 +38,21 @@ function start(url){
 }
 
 function exists(selector){
-    var existString = String.format("casper.then(function(){\nthis.assert({0});\n});", selector);
+    var existString = String.format("casper.then(function(){\nthis.assert({0});\n});\n", selector);
     console.log(existString);
 };
 
 function validate(selector, text){
-    var existString = String.format("casper.then(function(){\nthis.contains({0});\n});", selector, text);
+    var existString = String.format("casper.then(function(){\nthis.contains({0});\n});\n", selector, text);
     console.log(existString);
 };
 
 function click(selector){
-    var existString = String.format("casper.then(function(){\nthis.click({0});\n});", selector);
+    var existString = String.format("casper.then(function(){\nthis.click({0});\n});\n", selector);
     console.log(existString);
 };
 
 function fill(selector, text){
-    var existString = String.format("this.sendKeys(json['selector'], json['text']);", selector);
+    var existString = String.format("this.sendKeys(json['selector'], json['text']);\n", selector);
     console.log(existString);
 };
