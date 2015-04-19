7795425b87fc1f248049b1175cbc31b085855ea4
Josh Waldron <jwaldron@thejwal.com>
2015-04-18 21:50:18 -0500

    Removed unused angularjs code

--- src/app/controllers/checksController.js
+++
@@ -1,7 +0,0 @@
-angular.module('ChecksApi.controllers', []).
-    controller('checksController', function($scope, checksAPIService) {
-        $scope.data = [];
-        checksAPIService.getChecks().success(function (response) {
-            $scope.data = response;
-        });
-    });
