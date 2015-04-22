1b1e9460711e996b00c5e02f622655e019167659
Josh Waldron <jwaldron@thejwal.com>
2015-04-21 23:43:38 -0500

    Fixed exists.js test

--- src/siteTests/exists.js
+++ src/siteTests/exists.js
@@ -2,7 +2,7 @@ casper.test.begin('Google test, ensure logo exists', 1, function suite(test){
     casper.start('http://google.com');
 
     casper.then(function(){
-        test.asserExists('img#hplogo123')
+        test.assertExists('img#hplogo123')
     });
 
     casper.run(function(){
